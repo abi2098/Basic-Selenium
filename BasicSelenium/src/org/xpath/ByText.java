49b0f0425179a68b3e09480334cc874825231c4c
abi2098 <abiramikumaresan98@gmail.com>
2024-10-28 16:28:26 +0530

    update

--- BasicSelenium/src/org/xpath/ByText.java
+++
@@ -1,7 +0,0 @@
-package org.xpath;
-
-public class ByText {
-public static void main(String[] args) {
-	System.out.println("Hello I am abi");
-}
-}
---
+++ BasicSelenium/src/org/xpath/GetText.java
@@ -0,0 +1,7 @@
+package org.xpath;
+
+public class GetText {
+public static void main(String[] args) {
+	System.out.println("Hi I am main method");
+}
+}
